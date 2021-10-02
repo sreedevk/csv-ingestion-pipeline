e91c3122e91abccf96e9c29b762cf8d69d164903
sreedev <sreedevpadmakumar@gmail.com>
2021-10-03 02:02:50 +0530

    fine tuning ingestion pipeline

--- lib/genre_matcher/ingestor/pipeline.ex
+++ lib/genre_matcher/ingestor/pipeline.ex
@@ -12,7 +12,7 @@ defmodule GenreMatcher.Ingestor.Pipeline do
       __MODULE__,
       name: __MODULE__,
       max_restarts: 3,
-      shutdown: 10_000,
+      shutdown: 5_000,
       producer: [
         module: {GenreMatcher.Ingestor.FileReader, opts},
         transformer: {GenreMatcher.Ingestor.MessageGenerator, :generate, []},
@@ -20,23 +20,23 @@ defmodule GenreMatcher.Ingestor.Pipeline do
       ],
       processors: [
         default: [
-          min_demand: 10,
-          max_demand: 50,
-          concurrency: 5
+          min_demand: 50_000,
+          max_demand: 100_000,
+          concurrency: 4
         ]
       ],
       batchers: [
         # DEFAULT BATCHER
         default: [
           concurrency: 1,
-          batch_size: 10,
+          batch_size: 100_000,
           batch_timeout: 100,
         ],
         # BATCHER - BATCHES MOVIES BASED ON YEAR & INSERTS INTO REDIS
-        dispatch_message: [
-          concurrency: 5,
-          batch_size: 50,
-          batch_timeout: 500
+        stream_dispatch: [
+          concurrency: 8,
+          batch_size: 200_000,
+          batch_timeout: 1_000
         ]
       ]
     )
@@ -61,14 +61,14 @@ defmodule GenreMatcher.Ingestor.Pipeline do
     |> Message.put_batch_key(batch_key)
   end
 
-  defp batching("movie.csv_record", nil),       do: { :dispatch_message, :default }
-  defp batching("movie.csv_record", batch_key), do: { :dispatch_message, batch_key }
+  defp batching("movie.csv_record", nil),       do: { :stream_dispatch, :default }
+  defp batching("movie.csv_record", batch_key), do: { :stream_dispatch, batch_key }
   defp batching(_event, _key),                  do: { :default, :default }
   defp batching(_opts),                         do: { :default, :default }
 
   # functions - BATCHER
   @impl Broadway
-  def handle_batch(:dispatch_message, messages, _batch_info, _context), do: batch_insert_redis(messages)
+  def handle_batch(:stream_dispatch, messages, _batch_info, _context), do: batch_insert_redis(messages)
   def handle_batch(_, messages, _), do: messages
 
   defp batch_insert_redis(batch) do
